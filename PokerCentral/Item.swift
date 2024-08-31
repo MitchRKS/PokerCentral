eb90527cc03b07e06613270a4d4f0e9acf650d8c
Mitch Rucker <mitchruckerks@gmail.com>
2024-08-31 02:46:10 -0500

    Initial Commit

---
+++ PokerCentral/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  PokerCentral
+//
+//  Created by Mitchell Rucker on 8/31/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ PokerCentral/PokerCentralApp.swift
@@ -0,0 +1,32 @@
+//
+//  PokerCentralApp.swift
+//  PokerCentral
+//
+//  Created by Mitchell Rucker on 8/31/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PokerCentralApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
